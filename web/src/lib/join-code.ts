4796c98896c016a6146efd393ceae013fc95be9a
Haochen Fa <billy.fa@qq.com>
2026-02-03 06:01:34 +0800

    fix(web): make join code generation uniform

--- web/src/lib/join-code.ts
+++ web/src/lib/join-code.ts
@@ -3,11 +3,10 @@ import crypto from "node:crypto";
 const ALPHABET = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789";
 
 export function generateJoinCode(length = 6) {
-  const bytes = crypto.randomBytes(length);
   let code = "";
 
   for (let i = 0; i < length; i += 1) {
-    const index = bytes[i] % ALPHABET.length;
+    const index = crypto.randomInt(0, ALPHABET.length);
     code += ALPHABET[index];
   }
 
